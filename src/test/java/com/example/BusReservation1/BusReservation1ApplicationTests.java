50372a47ded9a1f3736833f4f1da5a60f61697cb
wku-cslab1 <danielsemu06@gmail.com>
2023-04-30 12:50:34 +0300

    dfgd

---
+++ src/test/java/com/example/BusReservation1/BusReservation1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BusReservation1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusReservation1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
