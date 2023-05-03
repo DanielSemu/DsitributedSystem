65e480f878cab93a72d3d2a351c6f7ec154adba1
wku-cslab1 <danielsemu06@gmail.com>
2023-05-03 12:13:09 +0300

    commit2

---
+++ src/test/java/com/masai/BusReservation1/BusReservation1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masai.BusReservation1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusReservation1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
